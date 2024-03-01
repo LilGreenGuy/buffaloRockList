df3077cda817e00d669d7ff3f6119f333dfb2b81
Lil Green Guy <139634331+LilGreenGuy@users.noreply.github.com>
2024-03-01 15:17:08 -0600

    Creation commit. Basic HTML boilerplate and initial objects for the various drinks created.

---
+++ app.js
@@ -0,0 +1,31 @@
+const pepsiCo = [
+    {
+        brands: ['Pepsi', 'Mountain Dew', 'Starry', 'Gatorade', 'Aquafina', 'Propel', 'Lipton', 'Mug', 'Lifewater', 'Rockstar', 'SoBe'],
+        flavors: ['Wild Cherry',],
+        sizes: ['20oz', '2LT', '6pk', '8pk', '10pk', '12pk', '18pk', '24pk', '32pk'],
+        types: ['Original', 'Diet', 'Zero']
+    }
+]
+
+const keurigDrPepper = [
+    {
+        brands: ['Sundrop', 'Dr. Pepper', '7up', 'A&W', 'Canada Dry', 'Crush', 'Bai', 'Deja Blue', 'Hawaiian Punch', 'RC Cola', 'Diet Rite', 'Schweppes', 'Snapple', 'Squirt', 'Vernors'],
+        sizes: ['20oz', '2LT', '6pk', '8pk', '10pk', '12pk', '18pk', '24pk', '32pk'],
+        types: ['Original', 'Diet', 'Zero']
+    }
+]
+
+const buffaloRock = [
+    {
+        brands: ['Buffalo Rock Ginger Ale', 'Grapico', 'Sunfresh', 'Dr. Wham'],
+        sizes: ['20oz', '2LT', '6pk', '8pk', '10pk', '12pk', '18pk', '24pk', '32pk'],
+        types: ['Original', 'Diet', 'Zero']
+    }
+]
+const celsius = [
+    {
+        brands: 'Celsius',
+        sizes: ['12oz', '16oz', '12pk'],
+        types: ['Essentials', 'Live Fit']
+    }
+]
