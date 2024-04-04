f3894239e20883d7aa560e11236208a6995c922d
Lil Green Guy <139634331+LilGreenGuy@users.noreply.github.com>
2024-04-04 16:43:48 -0500

    Pushing to branch so I can work off of my home comp

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const pepsiCo = [
     {
-        brands: ['Pepsi', 'Mountain Dew', 'Starry', 'Gatorade', 'Aquafina', 'Propel', 'Lipton', 'Mug', 'Lifewater', 'Rockstar', 'SoBe'],
-        flavors: ['Wild Cherry',],
+        brand: ['Aquafina'],
+        flavors: null,
+        sizes: ['20oz', '1.25LT', '8pk', '24pk', '32pk'],
+        types: null
+    },
+    {
+        brand: ['Gatorade'],
+        flavors: ['Cool Blue', 'Cucumber Lime', 'Fruit Punch', 'Glacier Cherry', 'Glacier Freeze', 'Grape', 'Lemon Lime', 'Orange'],
+        sizes: ['20oz', '28oz', '128oz', '6pk', '8pk', '12pk', '18pk'],
+        types: ['Original', 'Zero']
+    },
+    {
+        brand: ['Pepsi', 'Mountain Dew', 'Starry', 'Gatorade', 'Aquafina', 'Propel', 'Lipton', 'Mug', 'Lifewater', 'Rockstar', 'SoBe'],
+        flavors: ['Baja', 'Code Red', 'Livewire', 'Voltage', 'Wild Cherry',],
         sizes: ['20oz', '2LT', '6pk', '8pk', '10pk', '12pk', '18pk', '24pk', '32pk'],
         types: ['Original', 'Diet', 'Zero']
     }
@@ -9,7 +21,9 @@ const pepsiCo = [
 
 const keurigDrPepper = [
     {
-        brands: ['Sundrop', 'Dr. Pepper', '7up', 'A&W', 'Canada Dry', 'Crush', 'Bai', 'Deja Blue', 'Hawaiian Punch', 'RC Cola', 'Diet Rite', 'Schweppes', 'Snapple', 'Squirt', 'Vernors'],
+        brand: ['AShoc', 'Sundrop', 'Dr. Pepper', '7up', 'A&W', 'Canada Dry', 'Crush', 'Bai', 'Deja Blue', 'Hawaiian Punch', 'RC Cola', 'Diet Rite',
+            'Schweppes', 'Snapple', 'Squirt', 'Vernors'],
+        flavors: ['Orange', 'Strawberry'],
         sizes: ['20oz', '2LT', '6pk', '8pk', '10pk', '12pk', '18pk', '24pk', '32pk'],
         types: ['Original', 'Diet', 'Zero']
     }
@@ -17,15 +31,23 @@ const keurigDrPepper = [
 
 const buffaloRock = [
     {
-        brands: ['Buffalo Rock Ginger Ale', 'Grapico', 'Sunfresh', 'Dr. Wham'],
+        brand: ['Buffalo Rock Ginger Ale', 'Grapico', 'Sunfresh'],
         sizes: ['20oz', '2LT', '6pk', '8pk', '10pk', '12pk', '18pk', '24pk', '32pk'],
         types: ['Original', 'Diet', 'Zero']
-    }
-]
-const celsius = [
+    },
     {
-        brands: 'Celsius',
-        sizes: ['12oz', '16oz', '12pk'],
-        types: ['Essentials', 'Live Fit']
+        brand: 'Dr Wham',
+        sizes: ['']
     }
 ]
+
+const celsius =
+{
+    brand: 'Celsius',
+    flavors: ['Arctic Vibe', 'Astro Vibe', 'Blue Crush', 'Blue Razz Lemonade', 'Cherry Limeade', 'Cosmic Vibe', 'Cola', 'Dragonberry', 'Fantasy Vibe',
+        'Fruit Burst', 'Fuji Apple Pear', 'Galaxy Vibe', 'Grape Rush', 'Green Apple Cherry', 'Kiwi Guava', 'Lemon Lime', 'Mango Passionfruit', 'Mango Tango',
+        'Oasis Vibe', 'Orange', 'Orangecicle', 'Peach Mango', 'Peach Vibe', 'Raspberry Acai', 'Raspberry Peach', 'Strawberry Guava', 'Strawberry Lemonade',
+        'Tropical Vibe', 'Watermelon'],
+    sizes: ['12oz', '16oz', '4pk', '12pk'],
+    types: ['Essentials', 'Live Fit']
+}
