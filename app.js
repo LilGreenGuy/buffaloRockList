0f0b6ebac652ad5be9411bf19f92a3d946b77bf0
Lil Green Guy <139634331+LilGreenGuy@users.noreply.github.com>
2024-04-11 10:54:39 -0500

    Added some steps to the list item creation process. Can no longer have no brand selected.

--- app.js
+++ app.js
@@ -19,34 +19,36 @@ const pepsiCo =
 // }
 
 
-const keurigDrPepper = [
-    {
-        brand: ['AShoc', 'Sundrop', 'Dr. Pepper', '7up', 'A&W', 'Canada Dry', 'Crush', 'Bai', 'Deja Blue', 'Hawaiian Punch', 'RC Cola', 'Diet Rite',
-            'Schweppes', 'Snapple', 'Squirt', 'Vernors'],
-        flavor: ['Cream Soda', 'Orange', 'Strawberry', 'Strawberries and Cream'],
-        size: ['20oz', '2LT', '6pk', '8pk', '10pk', '12pk', '18pk', '24pk', '32pk'],
-        variety: ['Original', 'Diet', 'Zero']
-    }
-]
+const keurigDrPepper =
+{
+    brand: ['AShoc', 'Sundrop', 'Dr. Pepper', '7up', 'A&W', 'Canada Dry', 'Crush', 'Bai', 'Deja Blue', 'Hawaiian Punch', 'RC Cola', 'Diet Rite',
+        'Schweppes', 'Snapple', 'Squirt', 'Vernors'],
+    flavor: ['Cream Soda', 'Orange', 'Strawberry', 'Strawberries and Cream'],
+    variety: [],
+    size: []
+}
+
 
 const buffaloRock =
 {
     brand: ['Buffalo Rock Ginger Ale', 'Dr Wham', 'Grapico', 'Sunfresh'],
-    sizes: ['20oz', '2LT', '6pk', '8pk', '10pk', '12pk', '18pk', '24pk', '32pk'],
-    Variety: ['Original', 'Diet', 'Zero']
+    flavor: [],
+    variety: [],
+    size: []
 }
 
 
 const celsius =
 {
-    brand: 'Celsius',
-    flavors: ['Arctic Vibe', 'Astro Vibe', 'Blue Crush', 'Blue Razz Lemonade', 'Cherry Limeade', 'Cosmic Vibe', 'Cola', 'Dragonberry', 'Fantasy Vibe',
+    brand: ['Celsius'],
+    flavor: ['Arctic Vibe', 'Astro Vibe', 'Blue Crush', 'Blue Razz Lemonade', 'Cherry Limeade', 'Cosmic Vibe', 'Cola', 'Dragonberry', 'Fantasy Vibe',
         'Fruit Burst', 'Fuji Apple Pear', 'Galaxy Vibe', 'Grape Rush', 'Green Apple Cherry', 'Kiwi Guava', 'Lemon Lime', 'Mango Passionfruit', 'Mango Tango',
         'Oasis Vibe', 'Orange', 'Orangecicle', 'Peach Mango', 'Peach Vibe', 'Raspberry Acai', 'Raspberry Peach', 'Strawberry Guava', 'Strawberry Lemonade',
         'Tropical Vibe', 'Watermelon'],
-    sizes: ['12oz', '16oz', '4pk', '12pk'],
-    types: ['Essentials', 'Live Fit']
+    variety: ['Essentials', 'Live Fit'],
+    size: ['12oz', '16oz', '4pk', '12pk']
 }
+
 const brandField = document.getElementById('brand');
 const flavorField = document.getElementById('flavor');
 const varietyField = document.getElementById('variety');
@@ -55,8 +57,13 @@ const submitBtn = document.querySelector('.submit')
 const listTable = document.querySelector('.table');
 const cases = document.getElementById('case');
 const units = document.getElementById('unit');
+// const deleteBtn = document.querySelectorAll('.deleteThis')
+const resetBtn = document.querySelector('.reset')
 
 submitBtn.onclick = () => addItem();
+resetBtn.onclick = () => resetList();
+// deleteBtn.onclick = () => deleteItem();
+
 
 function createBrands(objects) {
     for (i = 0; i < objects.brand.length; i++) {
@@ -84,16 +91,47 @@ function createBrands(objects) {
 function addItem() {
     const fillItem = document.createElement('div');
     fillItem.classList.add('todo');
-    listTable.appendChild(fillItem)
+    listTable.appendChild(fillItem);
 
     const listedItem = document.createElement('p');
     listedItem.classList.add('listItem');
-    listedItem.innerText = `${brandField.value} ${flavorField.value} ${varietyField.value} ${sizeField.value} needs ${cases.value} cases and ${units.value} units`
     fillItem.appendChild(listedItem);
+    if (brandField.value === '') {
+        const parent = listedItem.parentNode;
+        return parent.remove();
+    } else if ((cases.value === '0' || cases.value === '') && (units.value === '0' || units.value === '')) {
+            const parent = listedItem.parentNode;
+            return parent.remove();
+        } else if (cases.value === '0' || cases.value == '') {
+            listedItem.innerText = `${brandField.value} ${flavorField.value} ${varietyField.value} ${sizeField.value} needs ${units.value} units`
+        } else if (units.value === '0' || units.value === '') {
+            listedItem.innerText = `${brandField.value} ${flavorField.value} ${varietyField.value} ${sizeField.value} needs ${cases.value} cases`
+        } else if (cases.value > 0 && units.value > 0) {
+            listedItem.innerText = `${brandField.value} ${flavorField.value} ${varietyField.value} ${sizeField.value} needs ${cases.value} cases and ${units.value} units`
+        }
+    
 
     const deleteBtn = document.createElement('button');
     deleteBtn.classList.add('deleteThis');
+    deleteBtn.innerText = 'X'
+    deleteBtn.style.color = 'red'
     fillItem.appendChild(deleteBtn);
+
+    function deleteItem() {
+
+        const parent = deleteBtn.parentNode;
+        parent.remove();
+    }
+
+    deleteBtn.onclick = () => deleteItem();
 }
 
+function resetList() {
+    listTable.innerHTML = ''
+}
+
+
 createBrands(pepsiCo)
+createBrands(keurigDrPepper)
+createBrands(buffaloRock)
+createBrands(celsius)
